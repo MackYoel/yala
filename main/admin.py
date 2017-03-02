93375969b25556ede4b32d7ae87a762694c801ca
Yoel <mack.zalas@gmail.com>
2017-03-02 08:48:04 -0500

    add Session to admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import KnowledgeArea, Person, Theme, Issue
+from .models import KnowledgeArea, Person, Theme, Issue, Session
 
 
 class KnowledgeAreaAdmin(admin.ModelAdmin):
@@ -18,7 +18,13 @@ class IssueAdmin(admin.ModelAdmin):
     list_display = ('name', 'theme', 'completed', 'doing', 'created_at')
 
 
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('issue', 'pk', 'starts_at', 'ends_at', 'active',
+                    'closed_to_force')
+
+
 admin.site.register(KnowledgeArea, KnowledgeAreaAdmin)
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Theme, ThemeAdmin)
 admin.site.register(Issue, IssueAdmin)
+admin.site.register(Session, SessionAdmin)
